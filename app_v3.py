810295e4fb8e4d3aa08e6b82d633fa4960803bcf
petr1shilov <71299844+petr1shilov@users.noreply.github.com>
2024-03-26 18:21:34 +0300

    новая проверка

--- app_v3.py
+++ app_v3.py
@@ -51,143 +51,147 @@ def embeding(text_query, text_links, tresh_hold=tresh_hold):
 
     return list_of_candidates
 
-def get_token(auth_token, scope='GIGACHAT_API_PERS'):
-    """
-      Выполняет POST-запрос к эндпоинту, который выдает токен.
+# def get_token(auth_token, scope='GIGACHAT_API_PERS'):
+#     """
+#       Выполняет POST-запрос к эндпоинту, который выдает токен.
 
-      Параметры:
-      - auth_token (str): токен авторизации, необходимый для запроса.
-      - область (str): область действия запроса API. По умолчанию — «GIGACHAT_API_PERS».
+#       Параметры:
+#       - auth_token (str): токен авторизации, необходимый для запроса.
+#       - область (str): область действия запроса API. По умолчанию — «GIGACHAT_API_PERS».
 
-      Возвращает:
-      - ответ API, где токен и срок его "годности".
-      """
-    # Создадим идентификатор UUID (36 знаков)
-    rq_uid = str(uuid.uuid4())
+#       Возвращает:
+#       - ответ API, где токен и срок его "годности".
+#       """
+#     # Создадим идентификатор UUID (36 знаков)
+#     rq_uid = str(uuid.uuid4())
 
-    # API URL
-    url = "https://ngw.devices.sberbank.ru:9443/api/v2/oauth"
+#     # API URL
+#     url = "https://ngw.devices.sberbank.ru:9443/api/v2/oauth"
 
-    # Заголовки
-    headers = {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Accept': 'application/json',
-        'RqUID': rq_uid,
-        'Authorization': f'Basic {auth_token}'
-    }
+#     # Заголовки
+#     headers = {
+#         'Content-Type': 'application/x-www-form-urlencoded',
+#         'Accept': 'application/json',
+#         'RqUID': rq_uid,
+#         'Authorization': f'Basic {auth_token}'
+#     }
 
-    # Тело запроса
-    payload = {
-        'scope': scope
-    }
+#     # Тело запроса
+#     payload = {
+#         'scope': scope
+#     }
 
-    try:
-        # Делаем POST запрос с отключенной SSL верификацией
-        # (можно скачать сертификаты Минцифры, тогда отключать проверку не надо)
-        response = requests.post(url, headers=headers, data=payload, verify=False)
-        return response
-    except requests.RequestException as e:
-        print(f"Ошибка: {str(e)}")
-        return -1
+#     try:
+#         # Делаем POST запрос с отключенной SSL верификацией
+#         # (можно скачать сертификаты Минцифры, тогда отключать проверку не надо)
+#         response = requests.post(url, headers=headers, data=payload, verify=False)
+#         return response
+#     except requests.RequestException as e:
+#         print(f"Ошибка: {str(e)}")
+#         return -1
     
 
-def get_chat_completion(auth_token, user_message):
-    """
-    Отправляет POST-запрос к API чата для получения ответа от модели GigaChat.
+# def get_chat_completion(auth_token, user_message):
+#     """
+#     Отправляет POST-запрос к API чата для получения ответа от модели GigaChat.
 
-    Параметры:
-    - auth_token (str): Токен для авторизации в API.
-    - user_message (str): Сообщение от пользователя, для которого нужно получить ответ.
+#     Параметры:
+#     - auth_token (str): Токен для авторизации в API.
+#     - user_message (str): Сообщение от пользователя, для которого нужно получить ответ.
 
-    Возвращает:
-    - str: Ответ от API в виде текстовой строки.
-    """
-    # URL API, к которому мы обращаемся
-    url = "https://gigachat.devices.sberbank.ru/api/v1/chat/completions"
+#     Возвращает:
+#     - str: Ответ от API в виде текстовой строки.
+#     """
+#     # URL API, к которому мы обращаемся
+#     url = "https://gigachat.devices.sberbank.ru/api/v1/chat/completions"
 
-    # Подготовка данных запроса в формате JSON
-    payload = json.dumps({
-        "model": "GigaChat:latest",  # Используемая модель
-        "messages": [
-            {
-                "role": "user",  # Роль отправителя (пользователь)
-                "content": user_message  # Содержание сообщения
-            }
-        ],
-        "temperature": 1,  # Температура генерации
-        "top_p": 0.1,  # Параметр top_p для контроля разнообразия ответов
-        "n": 1,  # Количество возвращаемых ответов
-        "stream": False,  # Потоковая ли передача ответов
-        "max_tokens": 512,  # Максимальное количество токенов в ответе
-        "repetition_penalty": 1,  # Штраф за повторения
-        "update_interval": 0  # Интервал обновления (для потоковой передачи)
-    })
+#     # Подготовка данных запроса в формате JSON
+#     payload = json.dumps({
+#         "model": "GigaChat:latest",  # Используемая модель
+#         "messages": [
+#             {
+#                 "role": "user",  # Роль отправителя (пользователь)
+#                 "content": user_message  # Содержание сообщения
+#             }
+#         ],
+#         "temperature": 1,  # Температура генерации
+#         "top_p": 0.1,  # Параметр top_p для контроля разнообразия ответов
+#         "n": 1,  # Количество возвращаемых ответов
+#         "stream": False,  # Потоковая ли передача ответов
+#         "max_tokens": 512,  # Максимальное количество токенов в ответе
+#         "repetition_penalty": 1,  # Штраф за повторения
+#         "update_interval": 0  # Интервал обновления (для потоковой передачи)
+#     })
 
-    # Заголовки запроса
-    headers = {
-        'Content-Type': 'application/json',  # Тип содержимого - JSON
-        'Accept': 'application/json',  # Принимаем ответ в формате JSON
-        'Authorization': f'Bearer {auth_token}'  # Токен авторизации
-    }
+#     # Заголовки запроса
+#     headers = {
+#         'Content-Type': 'application/json',  # Тип содержимого - JSON
+#         'Accept': 'application/json',  # Принимаем ответ в формате JSON
+#         'Authorization': f'Bearer {auth_token}'  # Токен авторизации
+#     }
 
-    # Выполнение POST-запроса и возвращение ответа
-    try:
-        response = requests.request("POST", url, headers=headers, data=payload, verify=False)
-        return response
-    except requests.RequestException as e:
-        # Обработка исключения в случае ошибки запроса
-        print(f"Произошла ошибка: {str(e)}")
-        return -1
+#     # Выполнение POST-запроса и возвращение ответа
+#     try:
+#         response = requests.request("POST", url, headers=headers, data=payload, verify=False)
+#         return response
+#     except requests.RequestException as e:
+#         # Обработка исключения в случае ошибки запроса
+#         print(f"Произошла ошибка: {str(e)}")
+#         return -1
 
 
-def main(text_query, links):
+# def main(text_query, links):
 
-    text_links_prep = prepare_links(links)
+#     text_links_prep = prepare_links(links)
 
-    text_links = embeding(text_query, text_links_prep)
+#     text_links = embeding(text_query, text_links_prep)
 
-    st.write(text_links)
+#     response = get_token(auth)
+#     if response != -1:
+#         giga_token = response.json()['access_token']
 
-    # response = get_token(auth)
-    # if response != -1:
-    #     giga_token = response.json()['access_token']
+#     answer = {}
 
-    # answer = {}
-
-    # for i in text_links:
-    #     text_n = text_links[i]
-    #     text_for_api = f'Ответь да или нет, есть ли подтверждение текста А в тексте Б \nТекст А:\n{text_query}\nТекст Б:\n{text_n}'
-    #     answer_n = get_chat_completion(giga_token, text_for_api)
-    #     answer[i] = str(answer_n.json()['choices'][0]['message']['content'])
-    #     print(answer_n.json()['choices'][0]['message']['content'], text_n)
+#     for i in text_links:
+#         text_n = text_links[i]
+#         text_for_api = f'Ответь да или нет, есть ли подтверждение текста А в тексте Б \nТекст А:\n{text_query}\nТекст Б:\n{text_n}'
+#         answer_n = get_chat_completion(giga_token, text_for_api)
+#         answer[i] = str(answer_n.json()['choices'][0]['message']['content'])
+#         print(answer_n.json()['choices'][0]['message']['content'], text_n)
 
     
-    # for i in range(len(text_links_prep)):
-    #     st.write(f'({i}) --> {text_links_prep[i]}')
+#     for i in range(len(text_links_prep)):
+#         st.write(f'({i}) --> {text_links_prep[i]}')
 
 
 
-    # return answer, text_links_prep
+#     return answer, text_links_prep
+
+# st.title('App for validation links')
+# query = st.text_area('Text from article')
+# links = st.text_area('Text from link')  
+
+# if st.button('Tap to submit'):
+
+#     answer, text_links = main(query, links)
+
+#     st.title('Ответы LLM для предложений-кандидатов')
+
+#     for i in answer:
+#         st.write(f'({i}) --> предложение-кандитат {text_links[i]}')
+#         if answer[i] == 'Да':
+#             st.success(f'--> ответ модели: {answer[i]}')
+#         else:
+#             st.error(f'--> ответ модели: {answer[i]}')
+
 
 st.title('App for validation links')
 query = st.text_area('Text from article')
-links = st.text_area('Text from link')  
-
-if st.button('Tap to submit'):
-
-    # answer, text_links = main(query, links)
-    main(query, links)
-    # st.title('Ответы LLM для предложений-кандидатов')
-
-    # for i in answer:
-    #     st.write(f'({i}) --> предложение-кандитат {text_links[i]}')
-    #     if answer[i] == 'Да':
-    #         st.success(f'--> ответ модели: {answer[i]}')
-    #     else:
-    #         st.error(f'--> ответ модели: {answer[i]}')
+links = st.text_area('Text from link')
 
 
+text_links_prep = prepare_links(links)
 
+text_links = embeding(query, text_links_prep)
 
-
-
+st.write(text_links)
